b4ad37e71a08c29f9e9e4476fba3e96de4554101
njuguna <rihanna8njuguna@gmail.com>
2024-04-12 11:23:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const searchBox = document.querySelector(`.searchBox`)
+const searchBtn = document.querySelector(`.searchBtn`)
+const recipeContainer = document.querySelector(`.recipe-container`)
+
+//Function to get Recipes
+recipeContainer.innerHTML = "<h2>Fetching Recipes...</h2>";
+const fetchRecipes = async (query) =>{
+    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const response = await data.json();
+
+recipeContainer.innerHTML = "";
+response.meals.forEach(meal => {
+    const recipeDiv = document.createElement(`div`);
+    recipeDiv.classList.add(`recipe`);
+    recipeDiv.innerHTML =`
+    <img src="${meal.strMealThumb}">
+    <h3>${meal.strMeal}</h3>
+    <p><span>${meal.strArea}</span> Dish</p>
+    <p>Belongs to <span>${meal.strCategory}</span> Category</p>\
+    `
+   
+    const button = document.createElement(`button`);
+    button.textContent = "View Recipe";
+    recipeDiv.appendChild(button);
+    recipeContainer.appendChild(recipeDiv);
+});
+}
+searchBtn.addEventListener(`click`, (e) =>{
+    e.preventDefault();
+    const searchInput = searchBox.value.trim ();
+    // console.log(searchInput)
+    fetchRecipes(searchInput);
+   // console.log("Button Clicked");
+});
