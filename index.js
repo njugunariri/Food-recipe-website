ecf14b28adc7320f35308cf7d21dca58389e9a56
njuguna <rihanna8njuguna@gmail.com>
2024-04-12 17:29:51 +0300

    eleventh

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ searchBtn.addEventListener(`click`, (e) =>{
     e.preventDefault();
     const searchInput = searchBox.value.trim ();
    if (!searchInput) {
-    recipeContainer.innerHTML = `<h2>Type the meal in the search box.`;
+    recipeContainer.innerHTML = `<h2>Type the meal in the search box.</h2>`;
     return;
    }
     fetchRecipes(searchInput);
